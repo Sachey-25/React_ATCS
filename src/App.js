35192c46f6ec3cb9e9845afcaa2c7eb8e4745cba
Sachey-25 <42082775+Sachey-25@users.noreply.github.com>
2022-08-05 10:56:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Register from './Register';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome again</h2>
+      <Register />
+    </div>
+  );
+}
+export default App;
